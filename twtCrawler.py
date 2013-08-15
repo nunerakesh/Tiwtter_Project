b1116fe6b9baefcbca4bf4ea18e27cb1b781ecfc
Kaiqun <fukaiqun@gmail.com>
2013-08-15 16:53:08 -0400

    update

--- twtCrawler.py
+++ twtCrawler.py
@@ -1,13 +1,20 @@
 import twitter
+import time
 
 TwitterAccountPool = [line.strip() for line in open('TwitterAccountList', 'r')]
+KeyWordsList = open('Keywords', 'r').read().split('\t')
 
 AccountToken = 0
+KeywordToken = 0
 
-def NextAccount():
-	global AccountToken
-	lines = AccCount()
-	AccountToken = (AccountToken + 1) % (lines + 1)
+def NextAccount(AccNum):
+	api = twitter.Api(
+	    consumer_key=TwitterAccountPool[AccNum].split('\t')[0],
+	    consumer_secret=TwitterAccountPool[AccNum].split('\t')[1],
+	    access_token_key=TwitterAccountPool[AccNum].split('\t')[2],
+	    access_token_secret=TwitterAccountPool[AccNum].split('\t')[3]
+	)
+	return api
 
 def AccCount():
 	count = 0
@@ -16,25 +23,24 @@ def AccCount():
 		buffer = thefile.read(65536)
 		if not buffer:break
 		count += buffer.count('\n')
-	return count
+	return count + 1
 
 while True:
 	try:
-		writingFile = open('D:/Data/CrawledTwt.txt','w')
+		TwitterApiInstance = NextAccount(AccountToken)
 
-		api = twitter.Api(
-		    consumer_key=TwitterAccountPool[AccountToken].split('\t')[0],
-		    consumer_secret=TwitterAccountPool[AccountToken].split('\t')[1],
-		    access_token_key=TwitterAccountPool[AccountToken].split('\t')[2],
-		    access_token_secret=TwitterAccountPool[AccountToken].split('\t')[3]
-		)
+		while True:
+			temp = TwitterApiInstance.GetSearch(term=KeyWordsList[KeywordToken], geocode=(38.907231,-77.036483,'20mi'))
+			KeywordToken = (KeywordToken + 1) % len(KeyWordsList)
+			# time.sleep(0.5)
+			for tmp in temp:
+				# writingFile.write(str(tmp) + '\n')
+				print tmp
 
-		temp = api.GetSearch(term='wtop', geocode=(38.907231,-77.036483,'20mi'))
-		for tmp in temp:
-		    #writingFile.write(str(tmp) + '\n')
-			print tmp
 	except Exception, e:
-		Errotype = e[0][0]['code']
+		Errotype = e.message[0]['code']
 		if Errotype is 88:
+			AccountToken = (AccountToken + 1) % AccCount()
 			print 'Switch Account!'
-			NextAccount()
+		else:
+			print e
